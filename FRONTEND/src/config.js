27072ce33880655249468ae5cacfdd5bd91b6b6f
kritika1803 <chkasya2003@gmail.com>
2025-10-24 17:28:50 +0530

    Jenkins Project

--- FRONTEND/src/config.js
+++ FRONTEND/src/config.js
@@ -1,5 +1,6 @@
 const config = {
-    url: "http://localhost:8080/2300033235-backend"
-};
-
-export default config;
+    url: "http://localhost:8080/2300033235-backend"  
+  };
+  
+  export default config;
+  
